1530336b0fd6f4d5ab359a85f844a720a4138346
eggywardani <eggyandikawardani@gmail.com>
2020-11-09 23:08:34 +0700

    add validation input

--- app/src/main/java/com/eggy/mytask2/kalkulatorbmi/CalculatorActivity.kt
+++ app/src/main/java/com/eggy/mytask2/kalkulatorbmi/CalculatorActivity.kt
@@ -16,48 +16,58 @@ class CalculatorActivity : AppCompatActivity() {
         setContentView(R.layout.activity_calculator)
 
         btn_hitung.setOnClickListener {
-            val berat = edt_berat.text.toString().toInt()
-            val tinggi = edt_tinggi.text.toString().toInt() * 0.01
+            val berat = edt_berat.text.toString().trim()
+            val tinggi = edt_tinggi.text.toString().trim()
+            when {
+                tinggi.isEmpty() -> {
+                    edt_tinggi.error = "Tinggi Wajib diisi"
+                }
+                berat.isEmpty() -> {
+                    edt_berat.error = "Berat Wajib diisi"
+                }
+                else->{
+                    val tinggiMeter = tinggi.toInt() * 0.01
+                    val bmi = berat.toInt() / (tinggiMeter * tinggiMeter)
+                    val df = DecimalFormat("#.##")
+                    val bmiResult = df.format(bmi)
+                    if (rg_jekel.checkedRadioButtonId == R.id.rb_pria) {
+                        when {
+                            bmi < 18 -> {
+                                tv_hasil.text = "Kurus"
+                            }
+                            bmi in 18.0..25.0 -> {
+                                tv_hasil.text = "Normal"
+                            }
+                            bmi in 25.0..27.0 -> {
+                                tv_hasil.text = "Kegemukan"
+                            }
+                            bmi > 27 -> {
+                                tv_hasil.text = "Obesitas"
+                            }
 
-            val bmi = berat / (tinggi * tinggi)
-            val df = DecimalFormat("#.##")
-            val bmiResult = df.format(bmi)
+                        }
+                    } else if (rg_jekel.checkedRadioButtonId == R.id.rb_wanita){
+                        when {
+                            bmi < 17 -> {
+                                tv_hasil.text = "Kurus"
+                            }
+                            bmi in 17.0..23.0 -> {
+                                tv_hasil.text = "Normal"
+                            }
+                            bmi in 23.0..27.0 -> {
+                                tv_hasil.text = "Kegemukan"
+                            }
+                            bmi > 27 -> {
+                                tv_hasil.text = "Obesitas"
+                            }
 
-            if (rg_jekel.checkedRadioButtonId == R.id.rb_pria){
-                when{
-                    bmi <18 ->{
-                        tv_hasil.text = "Kurus"
+                        }
+
                     }
-                    bmi in 18.0..25.0 ->{
-                        tv_hasil.text = "Normal"
-                    }
-                    bmi in 25.0..27.0 ->{
-                        tv_hasil.text = "Kegemukan"
-                    }
-                    bmi > 27 ->{
-                        tv_hasil.text = "Obesitas"
-                    }
+                    tv_hasil_bmi.text = bmiResult.toString()
 
                 }
-            }else{
-                when{
-                    bmi <17 ->{
-                        tv_hasil.text = "Kurus"
-                    }
-                    bmi in 17.0..23.0 ->{
-                        tv_hasil.text = "Normal"
-                    }
-                    bmi in 23.0..27.0 ->{
-                        tv_hasil.text = "Kegemukan"
-                    }
-                    bmi > 27 ->{
-                        tv_hasil.text = "Obesitas"
-                    }
-
-                }
-
             }
-            tv_hasil_bmi.text = bmiResult.toString()
 
         }
 
